02efdec83fedc37e89b43d90a51037012269b50a
Henrique <coura.henrique@gmail.com>
2016-05-25 14:04:33 -0300

    youtube player

--- app/src/main/java/com/apps/coura/decomplicaapp/VideoPageFragment.java
+++ app/src/main/java/com/apps/coura/decomplicaapp/VideoPageFragment.java
@@ -3,14 +3,22 @@ package com.apps.coura.decomplicaapp;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.apps.coura.decomplicaapp.model.ModuleFactory;
 import com.apps.coura.decomplicaapp.model.VideoPage;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerFragment;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 import java.util.List;
 
@@ -22,6 +30,8 @@ public class VideoPageFragment extends Fragment{
     private static final String MOD_POSITION = "mod_position";
     private static final String POSITION = "position";
     private VideoPage mVideoPage;
+    private YouTubePlayerView mYouTubePlayerView;
+    private YouTubePlayerSupportFragment youTubePlayerFragment;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,8 +48,50 @@ public class VideoPageFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_video, container, false);
 
-        TextView videoUrl = (TextView)v.findViewById(R.id.video_page_text_view);
-        videoUrl.setText(mVideoPage.getVideoUrl());
+        youTubePlayerFragment = (YouTubePlayerSupportFragment) getChildFragmentManager().findFragmentById(R.id.youtube_player);
+        youTubePlayerFragment.initialize(getString(R.string.youtube_api_key), new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("sDC3OYLQCyo");
+                youTubePlayer.setPlayerStateChangeListener(new YouTubePlayer.PlayerStateChangeListener() {
+                    @Override
+                    public void onLoading() {
+
+                    }
+
+                    @Override
+                    public void onLoaded(String s) {
+
+                    }
+
+                    @Override
+                    public void onAdStarted() {
+
+                    }
+
+                    @Override
+                    public void onVideoStarted() {
+
+                    }
+
+                    @Override
+                    public void onVideoEnded() {
+                        Log.d("Video", "Video has ended");
+                    }
+
+                    @Override
+                    public void onError(YouTubePlayer.ErrorReason errorReason) {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        });
+
 
         return v;
     }
@@ -53,4 +105,18 @@ public class VideoPageFragment extends Fragment{
         fragment.setArguments(args);
         return fragment;
     }
+
+
+//    mYouTubePlayerView = (YouTubePlayerView)v.findViewById(R.id.youtube_player);
+//    mYouTubePlayerView.initialize(getString(R.string.youtube_api_key), new YouTubePlayer.OnInitializedListener() {
+//        @Override
+//        public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+//            youTubePlayer.loadVideo("sDC3OYLQCyo");
+//        }
+//
+//        @Override
+//        public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+//
+//        }
+//    });
 }
